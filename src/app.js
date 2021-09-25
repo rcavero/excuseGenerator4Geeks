0380d13835027ac5e1aa9edc79af59517c35aead
rcavero <ramoncaveroaras@gmail.com>
2021-09-25 11:20:35 +0000

    Hemos creado el script en app.js que muestra la excusa en el html

--- src/app.js
+++ src/app.js
@@ -8,4 +8,34 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  //let excuse = excuseGenerate();
+  //console.log(excuse);
+  //document.querySelector("#excuse").innerHTML = excuse;
+  document.querySelector("#excuse").innerHTML = excuseGenerate();
 };
+
+function excuseGenerate() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  return (
+    rand_element(who) +
+    " " +
+    rand_element(action) +
+    " " +
+    rand_element(what) +
+    " " +
+    rand_element(when)
+  );
+}
+
+function rand_element(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
---
+++ src/script.js
@@ -0,0 +1,5 @@
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+//alert("Funciona el script");
